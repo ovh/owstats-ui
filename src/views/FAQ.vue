6aeb0b328952458f61a6f29120c258ab3639f658
Lucas Chaigne <lucas.chaigne@ovhcloud.com>
2022-07-21 13:27:34 +0200

    feat(owstats.faq): display full FAQ in one page
    Signed-off-by: Lucas Chaigne <lucas.chaigne@ovhcloud.com>

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,28 +1,55 @@
 <template>
-  <b-card class="card-margin">
-    <FaqAccordion :items="myItems" />
-  </b-card>
+  <div>
+    <b-card
+      v-for="(category, i) in categories"
+      :key="`category-${i}`"
+      class="card-margin"
+    >
+      <h2>
+        {{ $t('faq.' + category) }}
+      </h2>
+      <div
+        v-for="(item, j) in categoryItems(category)"
+        :key="`item-${j}`"
+      >
+        <h3> {{ item.title }} </h3>
+        <p> {{ item.value }} </p>
+      </div>
+    </b-card>
+  </div>
 </template>
 
 <script>
-import FaqAccordion from '../components/dashboards/FaqAccordion'
 import faqFR from '../i18n/faq_fr.json'
 import faqEN from '../i18n/faq_en.json'
 
 export default {
   name: 'FAQ',
-  components: {
-    FaqAccordion
-  },
   data () {
     const userLanguage = (
       (navigator.languages && navigator.languages[0]) ||
           ''
     ).substr(0, 2)
     if (userLanguage === 'fr') {
-      return { myItems: faqFR }
+      return { items: faqFR }
     }
-    return { myItems: faqEN }
+    return { items: faqEN }
+  },
+  computed: {
+    categories () {
+      const uniqueCategories = this.items
+        .map(item => item.category)
+        .filter(
+          (category, index, categories) =>
+            categories.indexOf(category) === index
+        )
+      return uniqueCategories
+    }
+  },
+  methods: {
+    categoryItems (category) {
+      return this.items.filter(item => item.category === category)
+    }
   }
 }
 </script>
